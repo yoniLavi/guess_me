2882983fffc533a96170f013eeb4269a2b2ab6b4
Yoni Yalovitsky <yoni.yalovitsky@fewbytes.com>
2014-05-12 02:16:09 +0300

    much improvement

--- hello.py
+++ hello.py
@@ -4,6 +4,20 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def hello_world():
     return 'Hello World!'
 
+@app.route('/hello/<name>')
+def hello(name):
+    return("Hello " + name)
+
+@app.route('/bye/<name>')
+def bye(name):
+    return("Bye " + name)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return "404: You're funny, there's no such page", 404
+
+if __name__ == "__main__":
+    app.run(debug=True)
