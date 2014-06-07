6393cb10055af6eeb7712b5a4ff947d36c977166
Yoni Yalovitsky <yoni.yalovitsky@fewbytes.com>
2014-06-07 15:33:21 +0300

    first working version of guessing game

--- hello.py
+++ hello.py
@@ -1,29 +1,63 @@
-import os
 import random
 from flask import Flask, session, redirect, url_for, escape, request
 
+MAX_NUMBER = 1000  # maximum guessable number
 app = Flask(__name__)
 
 users_data = {}
 
+
+#helper_functions
+def new_average(old_average, current_guesses, games_previously_played):
+    new_total_guesses = old_average * games_previously_played + current_guesses
+    new_games_played = games_previously_played + 1.0
+    return new_total_guesses / new_games_played
+
+
+def create_new_user(new_username):
+        session['username'] = new_username
+        users_data[new_username] = {"games_played": 0,
+                                    "current_guesses": 0,
+                                    "average_guesses": 0,
+                                    "best_guesses": 10000000
+                                   }
+
+
+def finish_game(username):
+    session.pop('the_number', None)
+
+    user_dict = users_data[username]
+    user_dict["average_guesses"] = new_average(user_dict["average_guesses"],
+                                               user_dict["current_guesses"],
+                                               user_dict["games_played"])
+    user_dict["games_played"] += 1
+    if user_dict["current_guesses"] < user_dict["best_guesses"]:
+        user_dict["best_guesses"] = user_dict["current_guesses"]
+
+
+def start_new_game(username):
+    session['the_number'] = random.randrange(MAX_NUMBER + 1)
+    users_data[username]['current_guesses'] = 0
+
+
+#controllers:
 @app.route('/')
 def index():
-    if 'username' in session:
+    if 'username' in session and session['username'] in users_data:
         return 'Logged in as %s' % escape(session['username'])
 
     return redirect(url_for('login'))
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    return 'There is no such page, <a href="%s">go back to the game</a>' % url_for('game'), 404
 
 
 #guess the number using sessions; keep high scores in a server-side dict
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        session['username'] = request.form['username']
+        new_username = request.form['username']
+        if new_username in users_data:
+            return 'The username <b>%s</b> is already taken' % new_username, 401
+
+        create_new_user(new_username)
         return redirect(url_for('index'))
 
     return '''
@@ -50,26 +84,37 @@ def game():
 
     return str(session['the_number'])
 
+
 @app.route('/guess/<int:guess>')
 def guess(guess):
     if 'username' not in session:
         return redirect(url_for('login'))
 
+    username = session['username']
     if 'the_number' not in session:
-        session['the_number'] = random.randrange(1000)
+        start_new_game(username)
+
+    users_data[username]['current_guesses'] += 1
 
     if guess < session['the_number']:
-        return 'too low'
+        return 'Too low'
     elif guess > session['the_number']:
-        return 'too high'
+        return 'Too high'
     else:
-        session.pop('the_number', None)
-        return 'Awesome, you won!'
+        finish_game(username)
+        return 'Awesome, %s! You won the game after %s guesses!' % (
+            username, users_data[username]['current_guesses'])
 
-#TBD: dashboard with all logged in users. Their record guess number, average guess number and current guess number
+
 @app.route('/dashboard')
 def dashboard():
-    return '\n'.join(session.keys())
+    return str(users_data)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return 'There is no such page, <a href="%s">go back to the game</a>' % url_for('game'), 404
+
 
 if __name__ == "__main__":
     app.secret_key = 'noOneWillGuessThis'
